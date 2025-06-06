94fc274a6dd149bf40d55277a99dd621b6379c5b
Michael Harris <22866088+demib72@users.noreply.github.com>
2025-06-07 01:02:49 +0800

    Wrap script logic in main guard

--- run.py
+++ run.py
@@ -2,12 +2,17 @@ from stargen import StarSystem
 from stargen.utils.latexout import LatexWriter
 from stargen.utils.random_name import generate_random_name
 
-filename = f"{generate_random_name()}.tex"
-#testsystem = StarSystem()
-star = StarSystem()
-tex = LatexWriter(star, filename)
+def main():
+    filename = f"{generate_random_name()}.tex"
+    # testsystem = StarSystem()
+    star = StarSystem()
+    tex = LatexWriter(star, filename)
 
-star.print_info()
-tex.write()
+    star.print_info()
+    tex.write()
 
-print(filename)
+    print(filename)
+
+
+if __name__ == "__main__":
+    main()
